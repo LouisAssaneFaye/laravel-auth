cc1b4485b516659e286dd5a04968b02afad0d72d
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-08-28 18:44:46 +0200

    added both softdelete and recycle bin

--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDelete;
+
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDelete;
     protected $fillable = [
         'title',
         'content',
